18bd659e3d5eede3f6effde70fd857877ca18c56
zhangsan <1531966614@qq.com>
2019-11-23 13:30:40 +0800

    q2

--- day22_git/src/com/itheima/dao/UserDao.java
+++ day22_git/src/com/itheima/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.itheima.dao;
 public class UserDao {
     public void findUser(){
         System.out.println("这是一个测试方法。。。。");
+        System.out.println("1234...");
         System.out.println("123...");
     }
 }
